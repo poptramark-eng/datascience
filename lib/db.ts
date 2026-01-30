5d286f4576c79193e9e580a0a59fe8694934145e
poptramark-eng <poptramark@gmail.com>
2026-01-30 11:04:44 +0000

    ca update

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,11 @@
 import {Pool} from "pg";
-import fs from "fs";
-import path from "path";
+//import fs from "fs";
+//import path from "path";
 
 
 const connectionString=process.env.POSTGRES_URI as string;
-const caCert= fs.readFileSync(path.join(process.cwd(), "certs/ca.pem")).toString();
-
+//const caCert= fs.readFileSync(path.join(process.cwd(), "certs/ca.pem")).toString();
+const caCert = process.env.CA_CERT;
 export const pool = new Pool(
     {
     connectionString,
