5be69f84bc1c0c0b6edb2e401b42b0ee1b697bb4
poptramark-eng <poptramark@gmail.com>
2026-02-04 19:46:44 +0300

    Update fetch request in route.ts
    Removed revalidation option from fetch request.

--- app/api/v1/news/category/route.ts
+++ app/api/v1/news/category/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest)
    const category = url.searchParams.get('category');
 
 
- const data = await fetch(`https://newsdata.io/api/1/latest?apikey=pub_6efb91d69a504050a79358a3bf141ef4&prioritydomain=top&country=ke&category=${category}`,{next: {revalidate:60}});
+ const data = await fetch(`https://newsdata.io/api/1/latest?apikey=pub_6efb91d69a504050a79358a3bf141ef4&prioritydomain=top&country=ke&category=${category}`);
  const test = await data.json();
  console.log(typeof test.results);
  const results = test.results;
