e7f56419203f474e27089381f71bcebc16e0abe6
poptramark-eng <poptramark@gmail.com>
2026-02-04 16:12:51 +0000

    bug fix'

--- app/api/v1/test/route.ts
+++
@@ -1,9 +0,0 @@
-import {NextResponse} from 'next/server';
-import {valkey} from '@/lib/valkey';
-import bcrypt from 'bcrypt';
-import { json } from 'stream/consumers';
-
-export async function GET(){
-    const test = await valkey.hgetall('users:poptramark@gmail.com');
-    return NextResponse.json({message: test});
-}
